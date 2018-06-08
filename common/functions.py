98182ddd401dfb427959945dd500d8cbd6b10eb4
Revine Kim <revinekim@Revineui-MacBook-Pro.local>
2018-06-08 10:55:32 +0900

    first_init

---
+++ common/functions.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+def identify_function(x):
+    return x
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+
+def relu(x):
+    return np.maximum(0, x)
+
+def relu_grad(x):
+    return np.array(x > 0, dtype=np.int)
+
+def softmax(x):
+    x = x - np.max(x)
+    return np.exp(x) / np.sum(np.exp(x))
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+# def cross_entropy_error(y, t):
+#     delta = 1e-7
+#     return -1 * np.sum(t * np.log(y + delta))
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+    
+def softmax_loss(x,t):
+    y = softmax(x)
+    return cross_entropy_error(y, t)
